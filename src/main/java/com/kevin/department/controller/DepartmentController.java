cdb1a2dcf9a628cd2a1e2b068a0f6c0442e27d4c
Kevin <kaiwen.hu@hand-china.com>
2018-10-19 19:13:11 +0800

    s

---
+++ src/main/java/com/kevin/department/controller/DepartmentController.java
@@ -0,0 +1,15 @@
+package com.kevin.department.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class DepartmentController {
+
+    @RequestMapping("/world")
+    public String helloWorld(){
+
+        return "helloWorld";
+    }
+}
