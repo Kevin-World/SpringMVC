76d506d5857e1c0d0548aa657fdff2af29f5356c
Kevin-World <kevin_hubu@163.com>
2018-10-19 22:42:19 +0800

    2

--- src/main/java/com/kevin/department/controller/DepartmentController.java
+++ src/main/java/com/kevin/department/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DepartmentController {
 
     @RequestMapping("/world")
-    public String helloWorld(){
+    public String helloWorld() {
 
         return "helloWorld";
     }
